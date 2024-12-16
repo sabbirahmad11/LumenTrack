c2bb98ade6878aa09e0800a48a06b85c68204098
Mohammad Sabbir Ahmad <sabbir110294@gmail.com>
2024-12-16 18:54:28 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Resume Builder!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// src/components/Header.js
+import React from "react";
+
+function Header() {
+  return (
+    <header style={{ textAlign: "center", padding: "1rem", backgroundColor: "#282c34", color: "white" }}>
+      <h1>Resume Builder</h1>
+      <p>Collect information and generate your professional resume!</p>
+    </header>
+  );
+}
+
+export default Header;
